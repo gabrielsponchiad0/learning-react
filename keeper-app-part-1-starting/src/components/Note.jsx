f95bb17dfeb4c7930aaaf9c273e53e29e9f0421c
Gabriel <168313892+gabrielsponchiad0@users.noreply.github.com>
2025-12-29 03:37:20 -0300

    Add files via upload

---
+++ keeper-app-part-1-starting/src/components/Note.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Este é meu primeiro projeto!</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde magni
+        incidunt amet ipsum possimus laboriosam provident id omnis placeat
+        dolor.
+      </p>
+    </div>
+  );
+}
+
+export default Note;
---
+++ keeper-app-part-1-starting/src/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
+//1. Crie um novo aplicativo React.
+//2. Crie um componente App.jsx.
+//3. Crie um componente Header.jsx que renderize um elemento <header>
+//para exibir o nome do aplicativo Keeper em um <h1>.
+//4. Crie um componente Footer.jsx que renderize um elemento <footer>
+//para exibir uma mensagem de direitos autorais em um <p> com o ano atualizado dinamicamente.
+//5. Crie um componente Note.jsx para exibir um elemento <div> com um
+//<h1> para o título e um <p> para o conteúdo.
+//DICA: Você precisará estudar as classes no arquivo styles.css para aplicar a estilização.
